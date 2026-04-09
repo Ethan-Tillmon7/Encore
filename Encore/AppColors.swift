981c7c84128b9e0e461600050839ac6377b13116
Ethan-Tillmon7 <etillmon7@gmail.com>
2026-04-09 01:24:42 -0500

    feat: add adaptive color token system

---
+++ Encore/AppColors.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+// MARK: - SwiftUI Color tokens
+
+extension Color {
+    static let appBackground  = Color(uiColor: .appBackground)
+    static let appSurface     = Color(uiColor: .appSurface)
+    static let appAccent      = Color(uiColor: .appAccent)
+    static let appCTA         = Color(uiColor: .appCTA)
+    static let appTeal        = Color(uiColor: .appTeal)
+    static let appTextPrimary = Color(uiColor: .appTextPrimary)
+    static let appTextMuted   = Color(uiColor: .appTextMuted)
+}
+
+// MARK: - UIColor adaptive tokens
+
+extension UIColor {
+    static let appBackground = UIColor { t in
+        t.userInterfaceStyle == .dark
+            ? UIColor(appHex: "1C2522") : UIColor(appHex: "FAFDE6")
+    }
+    static let appSurface = UIColor { t in
+        t.userInterfaceStyle == .dark
+            ? UIColor(appHex: "2A332F") : UIColor(appHex: "FFFFF0")
+    }
+    static let appAccent      = UIColor(appHex: "A8BFB2")
+    static let appCTA         = UIColor(appHex: "E8F7D0")
+    static let appTeal        = UIColor(appHex: "D4ECEC")
+    static let appTextPrimary = UIColor { t in
+        t.userInterfaceStyle == .dark
+            ? UIColor(appHex: "EAEAEF") : UIColor(appHex: "202030")
+    }
+    static let appTextMuted   = UIColor(appHex: "A8BFB2")
+
+    convenience init(appHex: String) {
+        let val = UInt64(appHex, radix: 16) ?? 0
+        let r = CGFloat((val & 0xFF0000) >> 16) / 255
+        let g = CGFloat((val & 0x00FF00) >> 8)  / 255
+        let b = CGFloat(val & 0x0000FF)          / 255
+        self.init(red: r, green: g, blue: b, alpha: 1)
+    }
+}
